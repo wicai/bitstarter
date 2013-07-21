c71543f882795366da8735b95899a8bd915c8c70
WiCai <caimaster1@gmail.com>
2013-07-21 14:08:14 +0000

    fixed semicolon

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!';)
+  response.send('Hello World2!')
 });
 
 var port = process.env.PORT || 5000;
