45d047a1aed766312b8ac0582a3b11365718b448
WiCai <caimaster1@gmail.com>
2013-07-21 14:13:39 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+response.send(fs.readFileSync(index.html).toString());
+
 });
 
 var port = process.env.PORT || 5000;
